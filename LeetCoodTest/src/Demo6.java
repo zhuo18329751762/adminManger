2dfa51086c546d4ab3dc2a1f6e51634858a52a5d
zhuo18329751762 <2199698862@qq.com>
2023-02-27 21:04:48 +0800

    力扣第六题N型第二种解法

--- LeetCoodTest/src/Demo6.java
+++ LeetCoodTest/src/Demo6.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
     将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
 
@@ -15,9 +19,45 @@ string convert(string s, int numRows);
      */
 public class Demo6 {
     public static void main(String[] args) {
+        String str = convert2("PAYPALISHIRING", 3);
+        System.out.println(str);
+    }
+    public static String convert(String s, int numRows) {
+        if(numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for(char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if(i == 0 || i == numRows -1) flag = - flag;
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row : rows) res.append(row);
+        return res.toString();
 
     }
-
+    public static String convert2(String s, int numRows) {
+        if(numRows < 2) return s;
+        ArrayList<StringBuilder> list=new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            list.add(new StringBuilder());
+        }
+        int flag=-1;
+        int a=0;
+        for (int i = 0; i < s.length(); i++) {
+            list.get(a).append(s.charAt(i));
+            if(a==0||a==numRows-1){
+                flag=-flag;
+            }
+            a+=flag;
+        }
+        StringBuilder sb=new StringBuilder();
+        for (StringBuilder stringBuilder : list) {
+            sb.append(stringBuilder);
+        }
+        return sb.toString();
+    }
     public static String getString(String s, int numRows) {
         int n = s.length(), r = numRows;
         if (r == 1 || r >= n) {
