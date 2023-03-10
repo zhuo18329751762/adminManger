bfa6c612dc6cb70b7a582217afe2dd8ffd5fc9b2
zhuo18329751762 <2199698862@qq.com>
2023-03-10 21:31:47 +0800

    html css

---
+++ LeetCoodTest/src/Demo10.java
@@ -0,0 +1,16 @@
+public class Demo10 {
+    /*
+    给你一个字符串s和一个字符规律p，请你来实现一个支持 '.'和'*'的正则表达式匹配。
+
+'.' 匹配任意单个字符
+'*' 匹配零个或多个前面的那一个元素
+所谓匹配，是要涵盖整个字符串s的，而不是部分字符串
+     */
+    public static void main(String[] args) {
+
+    }
+    public static boolean isMatch(String s, String p) {
+
+        return false;
+    }
+}
---
+++ LeetCoodTest/src/Demo9.java
@@ -0,0 +1,28 @@
+public class Demo9 {
+    /*
+    给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+
+回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+例如，121 是回文，而 123 不是。
+     */
+    public static void main(String[] args) {
+        boolean flag = isPalindrome(0);
+        System.out.println(flag);
+    }
+    public static boolean isPalindrome(int x) {
+        if(x==0){
+            return true;
+        }
+        if(x<0||(x%10)==0&&x!=0){
+            return false;
+        }
+        int a=x;
+        int num=0;
+        while (x>0){
+            num=num*10+x%10;
+            x=x/10;
+        }
+        return a==num;
+    }
+}
