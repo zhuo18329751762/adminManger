be222e4f090100e077f69c83aa3fce730a50fe65
zhuo18329751762 <2199698862@qq.com>
2023-04-04 22:24:59 +0800

    LeetCood 20 21

---
+++ LeetCoodTest/src/Demo20.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class Demo20 {
+    /*
+    给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+每个右括号都有一个对应的相同类型的左括号。
+     */
+    public static void main(String[] args) {
+        System.out.println(isValid("([)]"));
+    }
+    public static boolean isValid(String s) {
+        if(s.isEmpty())
+            return true;
+        Stack<Character> stack=new Stack<Character>();
+        for(char c:s.toCharArray()){
+            if(c=='(')
+                stack.push(')');
+            else if(c=='{')
+                stack.push('}');
+            else if(c=='[')
+                stack.push(']');
+            else if(stack.empty()||c!=stack.pop())
+                return false;
+        }
+        if(stack.empty())
+            return true;
+        return false;
+    }
+}
---
+++ LeetCoodTest/src/Demo21.java
@@ -0,0 +1,66 @@
+public class Demo21 {
+    /*
+    将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     */
+    public static void main(String[] args) {
+        //l1 = [1,2,4], l2 = [1,3,4]
+        int[] arr1={1,2,4};
+        int[] arr2={1,3,4};
+        ListNode head1=new ListNode(0);
+        ListNode head2=new ListNode(0);
+        ListNode l1=head1;
+        ListNode l2=head2;
+        for (int i = 0; i < arr1.length; i++) {
+            l1.next=new ListNode(arr1[i]);
+            l1=l1.next;
+            //System.out.println(l1.val);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            l2.next=new ListNode(arr2[i]);
+            l2=l2.next;
+            //System.out.println(l2.val);
+        }
+        ListNode listNode = mergeTwoLists(head1.next, head2.next);
+        while (listNode!=null){
+            System.out.println(listNode.val);
+            listNode=listNode.next;
+        }
+    }
+
+        public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list1 == null) {
+                System.out.println(list2.val);
+                return list2;
+            }
+            else if (list2 == null) {
+                System.out.println(list1.val);
+                return list1;
+            }
+            else if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                System.out.println("l1"+list1.val);
+                return list1;
+            }
+            else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                System.out.println("l2"+list2.val);
+                return list2;
+            }
+        }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
