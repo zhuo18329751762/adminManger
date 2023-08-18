e0bc67cbc1b2829f9d44611a2c22499786058086
zhuo18329751762 <2199698862@qq.com>
2023-08-18 11:01:15 +0800

    力扣91

---
+++ LeetCoodTest/src/leetCood100/Demo91.java
@@ -0,0 +1,36 @@
+package leetCood100;
+
+public class Demo91 {
+    /*
+    一条包含字母 A-Z 的消息通过以下映射进行了 编码 ：
+'A' -> "1"
+'B' -> "2"
+...
+'Z' -> "26"
+要 解码 已编码的消息，所有数字必须基于上述映射的方法，反向映射回字母（可能有多种方法）。例如，"11106" 可以映射为：
+"AAJF" ，将消息分组为 (1 1 10 6)
+"KJF" ，将消息分组为 (11 10 6)
+注意，消息不能分组为  (1 11 06) ，因为 "06" 不能映射为 "F" ，这是由于 "6" 和 "06" 在映射中并不等价。
+给你一个只含数字的 非空 字符串 s ，请计算并返回 解码 方法的 总数 。
+题目数据保证答案肯定是一个 32 位 的整数。
+     */
+    public static void main(String[] args) {
+        System.out.println(numDecodings("1123"));
+    }
+    public static int numDecodings(String s) {
+        int n = s.length();
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            if (s.charAt(i - 1) != '0') {
+                f[i] += f[i - 1];
+                System.out.println("f["+i+"]="+f[i]);
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                f[i] += f[i - 2];
+            }
+
+        }
+        return f[n];
+    }
+}
