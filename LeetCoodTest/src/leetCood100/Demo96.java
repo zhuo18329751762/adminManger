54a561e9415c569b760e59061849aa482dee96f6
zhuo18329751762 <2199698862@qq.com>
2023-08-23 12:09:15 +0800

    力扣96

---
+++ LeetCoodTest/src/leetCood100/Demo96.java
@@ -0,0 +1,22 @@
+package leetCood100;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Demo96 {
+    /*
+    给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+     */
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+}
