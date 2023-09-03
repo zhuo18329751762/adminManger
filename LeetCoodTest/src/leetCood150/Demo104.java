c1c149b65d560071caf2eacf4eb504327739b3a3
zhuo18329751762 <2199698862@qq.com>
2023-09-03 17:13:20 +0800

    力扣104

---
+++ LeetCoodTest/src/leetCood150/Demo104.java
@@ -0,0 +1,21 @@
+package leetCood150;
+
+public class Demo104 {
+    /*
+    给定一个二叉树 root ，返回其最大深度。
+
+二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+     */
+
+    int count=0;
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+}
